6ce1ef632310b7ee2d3019006bfa0d7289bfc657
Tonyz1 <gianantonio.tosi@gmail.com>
2015-03-31 11:01:36 +0200

    vai

---
+++ lightbox/_vti_cnf/lightbox.js
@@ -0,0 +1,12 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TW|31 Jul 2011 14:23:37 -0000
+vti_author:SR|Gianantonio-PC\\Gianantonio
+vti_modifiedby:SR|Gianantonio-PC\\Gianantonio
+vti_nexttolasttimemodified:TW|31 Jul 2011 14:23:37 -0000
+vti_timecreated:TR|25 Jun 2013 14:15:55 -0000
+vti_cacheddtm:TX|25 Jun 2013 14:15:55 -0000
+vti_filesize:IR|12643
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|storia.htm
+vti_syncwith_ftp.impresaediletosi.it\:21:TW|25 Jun 2013 14:15:55 -0000
+vti_syncofs_ftp.impresaediletosi.it\:21:TW|31 Jul 2011 14:23:37 -0000
